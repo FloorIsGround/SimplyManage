a79852436168cccc96c26949d751766c342612f5
Jace <jrhesford@mail.fhsu.edu>
2026-04-04 23:03:34 -0500

    further slimming of useBookReviews after pushing review average logic to the db

--- react-ui/src/Components/Book/Reviews/useBookReviews.ts
+++ react-ui/src/Components/Book/Reviews/useBookReviews.ts
@@ -5,27 +5,17 @@ import type { Review } from "../../../Models/Book/Book";
 
 export function useBookReviews(bookId: string | null) {
     const [reviews, setReviews] = useState<Review[]>([]);
-    const [averageRating, setAverageRating] = useState<number>(0);
 
     useEffect(() => {
         if (!bookId) return;
 
         async function fetchReviews() {
             const res = await axiosServices.get(`/reviews/book/${bookId}`);
-            const fetchedReviews: Review[] = res.data;
-
-            setReviews(fetchedReviews);
-
-            if (fetchedReviews.length > 0) {
-                const avg = fetchedReviews.reduce((sum, r) => sum + r.rating, 0) / fetchedReviews.length;
-                setAverageRating(avg);
-            } else {
-                setAverageRating(0);
-            }
+            setReviews(res.data);
         }
 
         fetchReviews();
     }, [bookId]);
 
-    return { reviews, setReviews, averageRating };
+    return { reviews, setReviews };
 }
